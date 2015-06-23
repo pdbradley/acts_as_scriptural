ff57440debd8e7ae27f0789200e1d59a4db3bf4e
Philip Bradley <pdbradley@gmail.com>
2015-06-23 09:43:04 -0400

    fixed regex to allow for ranges spanning multiple books

--- lib/acts_as_scriptural/parser.rb
+++ lib/acts_as_scriptural/parser.rb
@@ -3,22 +3,33 @@ class ActsAsScriptural::Parser
 
   def self.parse_reference(reference)
     result = OpenStruct.new
-    regex = /^\s*([0-9]?\s*[a-zA-Z]+)\.?\s*([0-9]+)(?:\s*(?:-|..)[^0-9]*([0-9]+))?/
-    match = reference.match(regex)
+    # this reference needs to have no spaces to be parsed properly;
+    # the matches will contain (if applicable)
+    # match 1: first book name
+    # match 2: first chapter
+    # match 3: second book name (if present)
+    # match 4: last chapter
+    regex = /^([0-9]?[a-zA-Z]+)\.?([0-9]+)(?:-|..)?(?:([0-9]?[a-zA-Z]+)?\.?([0-9]+))?/
+    match = reference.gsub(/\s+/,'').match(regex)
     if match
-      if match[3]
-        result.first_book = match[1]
-        result.last_book = match[1]
-        result.first_chapter = match[2].to_i
-        result.last_chapter = match[3].to_i
-      else
+      if (match[3].nil? && match[4].nil?)  # only one chapter
         result.first_book = match[1]
         result.last_book = match[1]
         result.first_chapter = match[2].to_i
         result.last_chapter = match[2].to_i
+      elsif match[3]  # the range must include two books
+        result.first_book = match[1]
+        result.last_book = match[3]
+        result.first_chapter = match[2].to_i
+        result.last_chapter = match[4].to_i
+      else  # just one book in the range
+        result.first_book = match[1]
+        result.last_book = match[1]
+        result.first_chapter = match[2].to_i
+        result.last_chapter = match[4].to_i
       end
         result.first_book_index = ActsAsScriptural::AbbreviationLookup.new.index_number(result.first_book)
-        result.last_book_index = ActsAsScriptural::AbbreviationLookup.new.index_number(result.first_book)
+        result.last_book_index = ActsAsScriptural::AbbreviationLookup.new.index_number(result.last_book)
         result
     else
       nil
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -9,4 +9,28 @@ describe ActsAsScriptural::Parser do
     expect(result.last_book_index).to eq 1
     expect(result.last_chapter).to eq 1
   end
+  it "correctly parses a chapter range with ellipses from one book " do
+    result = ActsAsScriptural::Parser.parse_reference("Gen 1..3")
+
+    expect(result.first_book_index).to eq 1
+    expect(result.first_chapter).to eq 1
+    expect(result.last_book_index).to eq 1
+    expect(result.last_chapter).to eq 3
+  end
+  it "correctly parses a chapter range with a dash from one book " do
+    result = ActsAsScriptural::Parser.parse_reference("Gen 1-3")
+
+    expect(result.first_book_index).to eq 1
+    expect(result.first_chapter).to eq 1
+    expect(result.last_book_index).to eq 1
+    expect(result.last_chapter).to eq 3
+  end
+  it "correctly parses a chapter range with a dash from two books " do
+    result = ActsAsScriptural::Parser.parse_reference("Gen 1-Mat 28")
+
+    expect(result.first_book_index).to eq 1
+    expect(result.first_chapter).to eq 1
+    expect(result.last_book_index).to eq 40
+    expect(result.last_chapter).to eq 28
+  end
 end
